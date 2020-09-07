20be2273e3e0b5d8cbc92ea498b0f9b64f776cc9
Jay Quigley <quigley.james.g@gmail.com>
2020-09-07 10:34:56 -0700

    toward production seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,29 +1,34 @@
-5.times do
-  user = User.create(
-    first_name: Faker::Name.first_name,
-    last_name: Faker::Name.last_name,
-    email: Faker::Internet.email,
-  )
-  2.times do
-    recurrence_group = RecurrenceGroup.create(
-      user: user,
-      name: Faker::Game.title,
+if Rails.env.development?
+  5.times do
+    user = User.create(
+      first_name: Faker::Name.first_name,
+      last_name: Faker::Name.last_name,
+      email: Faker::Internet.email,
     )
-    3.times do
-      RecurrenceRule.create(
-        recurrence_group: recurrence_group,
-        day_of_week: RecurrenceRule.day_of_weeks.keys.sample,
-        frequency: "1 week",
-        start_time: "03:00",
-        end_time: "21:00",
-      )
-    end
-    5.times do
-      LinkSubscription.create(
-        recurrence_group: recurrence_group,
+    2.times do
+      recurrence_group = RecurrenceGroup.create(
         user: user,
-        url: Faker::Internet.url,
+        name: Faker::Game.title,
       )
+      3.times do
+        RecurrenceRule.create(
+          recurrence_group: recurrence_group,
+          day_of_week: RecurrenceRule.day_of_weeks.keys.sample,
+          frequency: "1 week",
+          start_time: "03:00",
+          end_time: "21:00",
+        )
+      end
+      5.times do
+        LinkSubscription.create(
+          recurrence_group: recurrence_group,
+          user: user,
+          url: Faker::Internet.url,
+        )
+      end
     end
   end
+end
+
+if Rails.env.production?
 end
