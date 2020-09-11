d4c93cdfeb8d326ae20b5b5109125919093db868
Jay Quigley <quigley.james.g@gmail.com>
2020-09-10 21:01:31 -0700

    Add LinkSubscriptionCreate mutation

---
+++ app/graphql/mutations/link_subscription_create.rb
@@ -0,0 +1,22 @@
+module Mutations
+  class LinkSubscriptionCreate < BaseMutation
+    argument :recurrence_group_id, ID, required: true
+    argument :url, String, required: true
+
+    field :link_subscription, Types::LinkSubscriptionType, null: true
+    field :errors, [String], null: false
+
+    def resolve(url:, recurrence_group_id:)
+      link_subscription = LinkSubscription.new(
+        recurrence_group_id: recurrence_group_id,
+        url: url,
+      )
+
+      if link_subscription.save
+        { link_subscription: link_subscription, errors: [] }
+      else
+        { link_subscription: nil, errors: link_subscription.errors.full_messages }
+      end
+    end
+  end
+end
