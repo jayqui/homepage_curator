3fb872a91bd05ec7c24f613bac6466cb8dfa78a3
Jay Quigley <quigley.james.g@gmail.com>
2020-09-12 09:43:37 -0700

    Add LinkSubscriptionDelete mutation

---
+++ app/graphql/mutations/link_subscription_delete.rb
@@ -0,0 +1,17 @@
+module Mutations
+  class LinkSubscriptionDelete < BaseMutation
+    argument :link_subscription_id, ID, required: true
+
+    field :link_subscription, Types::LinkSubscriptionType, null: true
+    field :errors, [String], null: false
+
+    def resolve(link_subscription_id:)
+      ls = LinkSubscription.find(link_subscription_id)
+      if ls.destroy
+        { link_subscription: ls, errors: [] }
+      else
+        { link_subscription: nil, errors: ls.errors.full_messages }
+      end
+    end
+  end
+end
